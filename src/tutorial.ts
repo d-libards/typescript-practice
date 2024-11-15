8e14a7f27fa4e0197314b49d49e99697fc337e49
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-16 03:22:02 +0800

    type annotation and type inference

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,36 @@
-console.log('typescript tutorial');
+// let awesomeName = 'shakeAndBake';
+// awesomeName = 'something';
+// awesomeName = awesomeName.toLocaleUpperCase();
+// console.log(awesomeName);
+
+// // awesomeName = 20;
+
+// let amount = 20;
+// amount = 20 - 1.52;
+// amount = parseFloat(amount.toFixed(2));
+// console.log(amount);
+
+// let isAwesome = true;
+// isAwesome = false;
+// console.log(isAwesome.valueOf());
+// // isAwesome = ' shakeAndBake';
+
+let name = 'DominiqUE';
+name = name.toLowerCase();
+console.log(name);
+// name = 20;
+
+let number = 20;
+number = number - 5;
+// number = 'Something';
+console.log(number);
+
+let isTruth = number < 12;
+isTruth = !isTruth;
+// isTruth = 20;
+
+if (isTruth) {
+  console.log('Truth');
+} else {
+  console.log('False');
+}
