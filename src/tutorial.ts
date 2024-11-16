c85a9209268db97e5d93dc5dd196e8c13899e0dd
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-16 21:39:47 +0800

    array and object

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,37 +1,57 @@
-let tax: number | string = 10;
-tax = 100;
-tax = '$100';
-// console.log(tax);
+// let prices: number[] = [100, 75, 42];
+// prices.push('hello')
 
-let requestStatus: 'pending' | 'success' | 'error' = 'pending';
-requestStatus = 'error';
-requestStatus = 'success';
-// requestStatus = 'random';
+// let fruits: string[] = ['apple, orange'];
 
-let notSure: any = 4;
-notSure = ' maybe a string instead';
-notSure = false;
+// let randomValues: [] = ['hello'];
 
-const books = ['1984', 'Brave New World', 'Celsius 45'];
+// let emptyValues: number[] = [];
 
-let foundBook: string | undefined;
+// let names = ['peter', 'susan', 1];
+// let array: (string | boolean)[] = ['apple', true, 'apple', false];
+// array.push(23);
 
-for (let book of books) {
-  if (book === '1983') {
-    foundBook = book;
-    foundBook = foundBook.toUpperCase();
-    break;
-  }
-}
-// foundBook = 10;
-console.log(foundBook);
-console.log(foundBook?.length);
+// let temperatures: number[] = [23, 43, 64, 23, 45];
+// // temperatures.push('temp');
 
-let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
-orderStatus = 'shipped';
-orderStatus = 'delivered';
-// orderStatus = 'cancelled';
+// let colors: string[] = ['blue', 'red', 'black'];
+// // colors.push(true);
 
-let discount: string | number = 20;
-discount = '20%';
-// discount = true;
+// let mixedArray: (string | number)[] = [23, 'red', 45, 'blue'];
+// // mixedArray.push(true);
+
+// let car: { brand: string; year: number; model: string } = {
+//   brand: 'honda',
+//   year: 2020,
+//   model: 'civic',
+// };
+// car.brand = 'ford';
+// // car.color = 'blue';
+
+// let car1: { brand: string; year: number; model: string } = {
+//   brand: 'ford',
+//   year: 2020,
+//   model: 'raptor',
+// };
+
+// let car2: { brand: string; year: number; model: string } = {
+//   brand: 'ford',
+// };
+// let book = { title: 'book', cost: 20 };
+// let pen = { title: 'pen', cost: 5 };
+// let notebook = { title: 'notebook' };
+
+// let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
+// items[0].title = 'new book';
+
+let bike: { brand: string; year: number } = { brand: 'Trek', year: 2020 };
+bike.year = 'Cannondale';
+
+let laptop: { brand: string; year: number } = { brand: 'Lenovo' };
+
+let product1 = { title: 'Tide', price: 25 };
+let product2 = { title: 'Tide2' };
+
+let arrayProducts: { title: string; price?: number }[] = [product1, product2];
+
+arrayProducts.push({ title: 'Champion', price: '$12' });
