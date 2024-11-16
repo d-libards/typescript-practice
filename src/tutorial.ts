91e3f7881839949b5eb03a3ca5ff58c7597ca0da
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-17 02:20:09 +0800

    function params and return

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,57 +1,44 @@
-// let prices: number[] = [100, 75, 42];
-// prices.push('hello')
+// function sayHi(name: string) {
+//   console.log(`Hello, ${name.toUpperCase()}`);
+// }
 
-// let fruits: string[] = ['apple, orange'];
+// sayHi('Dominique');
+// // sayHi(23);
 
-// let randomValues: [] = ['hello'];
+// function calculateDiscount(price: number) {
+//   const hasDiscount = true;
+//   if (hasDiscount) {
+//     return price.toFixed();
+//     // return 'Discount Applied';
+//   }
 
-// let emptyValues: number[] = [];
+//   return price * 0.9;
+// }
 
-// let names = ['peter', 'susan', 1];
-// let array: (string | boolean)[] = ['apple', true, 'apple', false];
-// array.push(23);
+// const finalPrice = calculateDiscount(200);
 
-// let temperatures: number[] = [23, 43, 64, 23, 45];
-// // temperatures.push('temp');
+const names = ['John', 'Dave', 'Micheal'];
 
-// let colors: string[] = ['blue', 'red', 'black'];
-// // colors.push(true);
+function checkName(name: string): boolean {
+  {
+    return names.includes(name);
+    // for (let arrNames of names) {
+    //   if (arrNames === name) {
+    //     return true;
+    //     break;
+    //   }
+    // }
+    // return false;
+  }
+}
 
-// let mixedArray: (string | number)[] = [23, 'red', 45, 'blue'];
-// // mixedArray.push(true);
+console.log(checkName('John'));
+console.log(checkName('Martha'));
 
-// let car: { brand: string; year: number; model: string } = {
-//   brand: 'honda',
-//   year: 2020,
-//   model: 'civic',
-// };
-// car.brand = 'ford';
-// // car.color = 'blue';
+let name = 'John';
 
-// let car1: { brand: string; year: number; model: string } = {
-//   brand: 'ford',
-//   year: 2020,
-//   model: 'raptor',
-// };
+console.log(`${name} is ${!checkName(name) ? 'not ' : ''}in the list.`);
 
-// let car2: { brand: string; year: number; model: string } = {
-//   brand: 'ford',
-// };
-// let book = { title: 'book', cost: 20 };
-// let pen = { title: 'pen', cost: 5 };
-// let notebook = { title: 'notebook' };
-
-// let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
-// items[0].title = 'new book';
-
-let bike: { brand: string; year: number } = { brand: 'Trek', year: 2020 };
-bike.year = 'Cannondale';
-
-let laptop: { brand: string; year: number } = { brand: 'Lenovo' };
-
-let product1 = { title: 'Tide', price: 25 };
-let product2 = { title: 'Tide2' };
-
-let arrayProducts: { title: string; price?: number }[] = [product1, product2];
-
-arrayProducts.push({ title: 'Champion', price: '$12' });
+if (checkName(name)) {
+  console.log(`${name} is ${!checkName(name) ? 'not ' : ''}in the list.`);
+}
