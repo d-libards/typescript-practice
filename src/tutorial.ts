03cb7e91b356862151c8cb8472c72c69b4e380f2
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-17 18:34:13 +0800

    interface

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,88 +1,25 @@
-// // type User = { id: number; name: string; isActive: boolean };
-// // const john: User = {
-// //   id: 1,
-// //   name: 'john',
-// //   isActive: true,
-// // };
-// // const susan: User = {
-// //   id: 1,
-// //   name: 'susan',
-// //   isActive: false,
-// // };
+interface Book {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+  //method
+  printAuthor(): void;
+  printTitle(message: string): string;
+}
 
-// // function createUser(user: User): User {
-// //   console.log(`Hello there ${user.name.toUpperCase()} !!!`);
-
-// //   return user;
-// // }
-
-// // type StringOrNumber = string | number;
-
-// // let value: StringOrNumber;
-// // value = 'hello';
-// // value = 123;
-
-// // type Theme = 'light' | 'dark';
-// // let theme: Theme;
-// // theme = 'dark';
-// // theme = 'light';
-
-// // function setTheme(t: Theme) {
-// //   theme = t;
-// // }
-
-// // setTheme('light');
-
-// type Employee = { id: number; name: string; department: string };
-
-// type Manager = { id: number; name: string; employees: Employee[] };
-
-// type Staff = Employee | Manager;
-
-// function printStaffDetails(staff: Staff): void {
-//   if ('employees' in staff) {
-//     console.log(
-//       `${staff.name} is an manager of ${staff.employees.length} employees`
-//     );
-//   } else {
-//     console.log(`${staff.name} is an employee in the ${staff.department}`);
-//   }
-// }
-
-// const alice: Employee = { id: 1, name: 'Alice', department: 'Finance' };
-// const maria: Employee = { id: 1, name: 'Alice', department: 'Human Resource' };
-
-// const bob: Manager = { id: 3, name: 'Bob', employees: [maria, alice] };
-
-// printStaffDetails(bob);
-// printStaffDetails(alice);
-
-// type Book = { id: number; name: string; price: number };
-// type DiscountedBook = Book & { discount: number };
-
-// const book1: Book = {
-//   id: 1,
-//   name: 'How to train your dragon',
-//   price: 200,
-// };
-
-// const book2: Book = {
-//   id: 2,
-//   name: 'Joke 101',
-//   price: 100,
-// };
-
-// const discountedBook: DiscountedBook = {
-//   id: 3,
-//   name: 'Love is War',
-//   price: 25,
-//   discount: 0.15,
-// };
-
-const propName = 'age';
-
-type Animal = {
-  [propName]: number;
+const deepWork: Book = {
+  isbn: 123456,
+  title: 'deep work',
+  author: 'Cal Newport',
+  genre: 'Self-help',
+  printAuthor() {
+    console.log(this.author);
+  },
+  printTitle(message) {
+    return `${message} ${this.title}`;
+  },
 };
 
-let tiger = { [propName]: 5 };
+deepWork.printAuthor();
+console.log(deepWork.printTitle('The title is'));
