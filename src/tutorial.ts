7b30fd0090ec97d3f4f9a2a2c8b2e61d42ce3667
d-libards <dominiquelibardo09@gmail.com>
2024-11-18 02:27:35 +0800

    interface fundamentals

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -6,6 +6,7 @@ interface Book {
   //method
   printAuthor(): void;
   printTitle(message: string): string;
+  printSomething: (someValue: number) => number;
 }
 
 const deepWork: Book = {
@@ -13,13 +14,53 @@ const deepWork: Book = {
   title: 'deep work',
   author: 'Cal Newport',
   genre: 'Self-help',
-  printAuthor() {
-    console.log(this.author);
-  },
+  // printAuthor() {
+  //   console.log(this.author);
+  // },
   printTitle(message) {
     return `${message} ${this.title}`;
   },
+  //first option
+  printSomething: function (someValue) {
+    return someValue;
+  },
+  //second op
+  // printSomething: (someValue) => {
+  //   // console.log(this);
+  //   console.log(deepWork.author);
+  //   return someValue;
+  // },
+  //third op
+  // printSomething(someValue) {
+  //   return someValue;
+  // },
+  printAuthor: () => {
+    console.log(deepWork.author);
+  },
 };
 
-deepWork.printAuthor();
-console.log(deepWork.printTitle('The title is'));
+// console.log(deepWork.printSomething(34));
+// deepWork.printAuthor();
+
+interface Computer {
+  readonly id: number;
+  brand: string;
+  ram: number;
+  storage?: number;
+  upgradeRam: (upgrade: number) => number;
+}
+
+const computer1: Computer = {
+  id: 1,
+  brand: 'lenovo',
+  ram: 16,
+  upgradeRam: function (amount) {
+    this.ram += amount;
+    return this.ram;
+  },
+};
+
+computer1.storage = 256;
+// computer1.id = 3;
+console.log(computer1.upgradeRam(4));
+console.log(computer1);
