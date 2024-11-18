3797dd56fefaf8f94be3d5aa1bca79a70d6ddcca
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-18 21:35:14 +0800

    type guarding - part 2

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,73 +1,65 @@
-type ValueType = string | number | boolean;
+type Student = {
+  name: string;
+  study: () => void;
+};
 
-let value: ValueType;
-const random = Math.random();
-value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
+type User = {
+  name: string;
+  login: () => void;
+};
 
-function checkValue(value: ValueType): void {
-  if (typeof value === 'string') {
-    console.log(value.toLowerCase());
-    return;
-  } else if (typeof value === 'number') {
-    console.log(value.toFixed(2));
-    return;
-  } else {
-    console.log(`boolean: ${value}`);
-    return;
-  }
+type Person = Student | User;
+
+const randomPerson = (): Person => {
+  return Math.random() > 0.5
+    ? { name: 'john', study: () => console.log('Studying') }
+    : { name: 'mary', login: () => console.log('Logging in') };
+};
+
+const person = randomPerson();
+
+function isStudent(person: Person): person is Student {
+  // return 'study' in person;
+  return (person as Student).study !== undefined;
 }
 
-// checkValue(value);
-type Dog = { type: 'dog'; name: string; bark: () => void };
-type Cat = { type: 'cat'; name: string; meow: () => void };
-type Animal = Dog | Cat;
-
-// function makeSound(animal: Animal): void {
-//   if (animal.type === 'dog') {
-//     animal.bark();
-//   } else {
-//     animal.meow();
-//   }
+// if (isStudent(person)) {
+//   person.study();
+// } else {
+//   person.login();
 // }
 
-// function makeSound(animal: Animal): void {
-//   if ('bark' in animal) {
-//     animal.bark();
-//   } else {
-//     animal.meow();
-//   }
-// }
+type IncrementAction = {
+  type: 'increment';
+  amount: number;
+  timestamp: number;
+  user: string;
+};
 
-function printLength(str: string | null | undefined): void {
-  if (str) {
-    console.log(str.length);
-  } else {
-    console.log('No string provided');
+type DecrementAction = {
+  type: 'decrement';
+  amount: number;
+  timestamp: number;
+  user: string;
+};
+
+type Action = IncrementAction | DecrementAction;
+
+function reducer(state: number, action: Action) {
+  switch (action.type) {
+    case 'increment':
+      return state + action.amount;
+    case 'decrement':
+      return state - action.amount;
+    default:
+      const unexpectedAction: never = action;
+      throw new Error(`Unexpected action: ${unexpectedAction}`);
   }
 }
 
-// printLength('qwerty');
-// printLength('');
-// printLength(null);
-try {
-  // throw 'some';
-  // throw new Error('This is an Error');
-} catch (error) {
-  if (error instanceof Error) {
-    console.log('Caught an Error Object: ' + error.message);
-  } else {
-    console.log('Caught an unknown error');
-  }
-}
-
-function checkInput(input: Date | string): string {
-  if (input instanceof Date) {
-    return input.getFullYear().toString();
-  }
-  return input;
-}
-
-const year = checkInput(new Date());
-const random2 = checkInput('2020-05-05');
-console.log(year);
-console.log(random2);
+const newState = reducer(15, {
+  user: 'John',
+  type: 'increment',
+  amount: 5,
+  timestamp: 1234567,
+});
