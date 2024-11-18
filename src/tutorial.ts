150068d8aff78ea788e37a9ba0ec78e9ebb8ff02
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-18 17:02:37 +0800

    type never

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,67 +1,41 @@
-let someValues: any = 'this is a string';
+// let someValues: never = 0;
 
-let strLength: number = (someValues as string).length;
+type Theme = 'light' | 'dark';
 
-type Bird = {
-  name: string;
-};
-
-let birdString = '{"name": "Eagle"}';
-let dogString = '{"breed": "Poodle"}';
-
-let birdObject = JSON.parse(birdString);
-let dogObject = JSON.parse(dogString);
-
-let bird = birdObject as Bird;
-let dog = dogObject as Bird;
-
-// console.log(bird.name);
-// console.log(dog.name);
-
-enum Status {
-  Pending = 'pending',
-  Declined = 'declined',
+function checkTheme(theme: Theme): void {
+  if (theme === 'light') {
+    console.log('light theme');
+    return;
+  }
+  if (theme === 'dark') {
+    console.log('dark theme');
+    return;
+  }
+  theme;
 }
 
-type User = {
-  name: string;
-  status: Status;
-};
-
-// save Status.Pending in the DB as a string
-// retrieve string from the DB
-const statusValue = 'pending';
-
-const user: User = { name: 'john', status: statusValue as Status };
-// console.log(user);
-
-let unknownValue: unknown;
-
-unknownValue = 'hello world';
-unknownValue = [1, 2, 3, 4, 5, 6];
-unknownValue = 23.32213;
-
-// unknownValue.toFixed(2);
-
-if (typeof unknownValue === 'number') {
-  unknownValue.toFixed(2);
+enum Color {
+  Red,
+  Blue,
+  Green,
 }
 
-function runSomeCode() {
-  const random = Math.random();
-  if (random < 0.5) {
-    throw new Error('there was an error....');
-  } else {
-    throw 'some error';
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return 'Red';
+    case Color.Blue:
+      return 'Blue';
+    case Color.Green:
+      return 'Green';
+    default:
+      // at built time
+      let unexpectedColor: never = color;
+      // at runtime
+      throw new Error(`Unexpected color value: ${color}`);
   }
 }
 
-try {
-  // runSomeCode();
-} catch (error) {
-  if (error instanceof Error) {
-    console.log(error.message);
-  } else {
-    console.log(error);
-  }
-}
+console.log(getColorName(Color.Blue));
+console.log(getColorName(Color.Red));
+console.log(getColorName(Color.Green));
